c1d45d21de75e3197e93e9985bec6df725c4a7cc
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-02-08 01:42:28 +0200

    Main Game UI Done

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.tictactoe.databinding.ActivityMainBinding
+import me.everything.android.ui.overscroll.OverScrollDecoratorHelper
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -11,8 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        setupScrollView()
         val playerName = intent.getStringExtra(PlayerNameActivity.PLAYER_NAME)
         Toast.makeText(this, playerName!!, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setupScrollView(){
+        OverScrollDecoratorHelper.setUpOverScroll(binding.scrollView)
     }
 }
